41d4c0dec10fc320079180aac4eec10b81315658
otoneumann <otarshenge@gmail.com>
2025-10-21 15:21:11 +0200

    ADD:layouts

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>Lara App</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+</head>
+<body>
+
+{{ $slot }}
+
+</body>
+</html>
