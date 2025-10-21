fbf28a77b5fab946c85d2de7e1660ae1138de186
otoneumann <otarshenge@gmail.com>
2025-10-21 15:21:41 +0200

    ADD:route

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::view('/home', 'home');
+Route::view('/', 'home');
 
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
