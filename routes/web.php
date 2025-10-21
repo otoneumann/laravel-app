77e5b4aaa65ae2b272ae2fc9f55e6f4e4edb02e2
otoneumann <otarshenge@gmail.com>
2025-10-21 14:56:51 +0200

    CHANGE:routes addjusted

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::view('/home', 'home');
+
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
