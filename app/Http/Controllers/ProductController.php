4b1080691e683d86aefdce038d10483adab3a38f
otoneumann <otarshenge@gmail.com>
2025-10-24 12:25:58 +0200

    CHANGE:ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SaveProductRequest;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -9,8 +10,46 @@ class ProductController extends Controller
 {
     public function index()
     {
-        $products = Product::all();
+        return view('products.index', [
+            'products' => Product::orderby('created_at')->paginate(3)
+        ]);
 
-        return view('products.index');
+    }
+
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    public function store(SaveProductRequest $request)
+    {
+        $product = Product::create($request->validated());
+
+        return redirect()->route('products.show', $product)->with('status', 'Product created!');
+    }
+
+    public function show(Product $product)
+    {
+        return view('products.show', compact('product'));
+    }
+
+    public function edit(Product $product)
+    {
+        return view('products.edit', compact('product'));
+    }
+
+    public function update(SaveProductRequest $request, Product $product)
+    {
+        $product->update($request->validated());
+
+        return redirect()->route('products.show', $product)->with('status', 'Product updated!');
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        return redirect()->route('products.index')
+            ->with('status', 'Product deleted successfully');
     }
 }
