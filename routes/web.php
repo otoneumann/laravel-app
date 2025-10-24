149e7ac3bf7d1b044ece7781c447f8ca5a5ee331
otoneumann <otarshenge@gmail.com>
2025-10-24 12:27:03 +0200

    CHANGE:Routes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,30 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 
-
 Route::view('/', 'home');
 
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::resource('categories', CategoryController::class);
+
+Route::controller(ProductController::class)
+    ->prefix('products')
+    ->name('product.')
+    ->group(function () {
+    Route::get('/', [ProductController::class, 'index'])->name('index');
+
+    Route::get('/create', [ProductController::class, 'create'])->name('create');
+
+    Route::post('/store', [ProductController::class, 'store'])->name('store');
+
+    Route::get('/{product}', [ProductController::class, 'show'])->name('show')->whereNumber('id');
+
+    Route::get('/{product}/edit', [ProductController::class, 'edit'])->name('edit');
+
+    Route::patch('/{product}', [ProductController::class, 'update'])->name('update');
+    Route::delete('/{product}', [ProductController::class, 'destroy'])->name('destroy');
+});
+
+
